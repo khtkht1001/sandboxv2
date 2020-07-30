25000c26cbcd7d68164f9cad377778f2adb5e2d1
Khant <khtkht1001@gmail.com>
2020-07-30 15:51:36 +0800

    Add word count function

---
+++ word_app.py
@@ -0,0 +1,11 @@
+"""Counting words"""
+
+def main():
+    print(count_words("A fox jumps over a ledge and falls flat"))
+
+def count_words(phrase):
+    words = phrase.split()
+    return len(words)
+
+if __name__ == '__main__':
+    main()
